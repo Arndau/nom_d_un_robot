57a1872077f8a24b951897cb01e4ce87e6db5b47
Le Garrec Arnaud <legarrecarnaud.92@gmail.com>
2022-05-11 20:42:14 +0200

    adding @name to initializer

--- robot_name.rb
+++ robot_name.rb
@@ -1,13 +1,17 @@
+
 class Robot
+
+  attr_accessor :name
+
+  def initialize
+    @name = [*('A'..'Z')].sample + [*('A'..'Z')].sample + [*(0..9)].sample.to_s + [*(0..9)].sample.to_s + [*(0..9)].sample.to_s
+  end
 
   def self.forget
   end
 
-  def name
-	[*('A'..'Z')].sample + [*('A'..'Z')].sample + [*(0..9)].sample.to_s + [*(0..9)].sample.to_s + [*(0..9)].sample.to_s
+  def reset
+    @name = [*('A'..'Z')].sample + [*('A'..'Z')].sample + [*(0..9)].sample.to_s + [*(0..9)].sample.to_s + [*(0..9)].sample.to_s
   end
 
-  def reset
-  end
-  
 end
